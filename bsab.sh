86f0beba3d66b2e90048f583e16ae32b4390a03a
mvidaldp <mvidal.dp@gmail.com>
2021-04-02 05:32:02 +0200

    Fix automatic width and height of dialog widget, add currency/symbol as parameter, right-justify values and headers

--- bsab.sh
+++ bsab.sh
@@ -18,7 +18,7 @@
 # all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
@@ -38,6 +38,10 @@
 # - comment whole code
 # - include error handling
 # - rewrite in functions
+# - quit using q/Q/ESC (read keyboard on background)
+# - try curl on multiprocessing
+# - fix right margin and line (check diff coins)
+# - reading parameters after first one (after $2)
 
 PROGNAME="bsab (Binance Spot Assets Balance)"
 SCRIPTNAME=${0##*/}
@@ -60,10 +64,14 @@ help_message() {
   Options:
 
   -h, --help        Display this help message and exit.
+  -c, --currency    Set currency symbol (e.g. EUR, BTC, ETH).
   -i, --investment  Set base investment (e.g. 1000, default=0).
 
 _EOF_
 }
+
+CURRENCY=EUR
+INVESTMENT=1000
 
 case $1 in
 
@@ -75,6 +83,14 @@ case $1 in
 --help)
   help_message
   exit
+  ;;
+
+-c)
+  CURRENCY=$2
+  ;;
+
+--currency)
+  CURRENCY=$2
   ;;
 
 -i)
@@ -110,7 +126,7 @@ UREVERSE="${PREFIX}R" # undo bg/fg text colors
 # underline="${PREFIX}u" # underline text
 # uunderline="${PREFIX}U" # undo underline text
 RESET="${PREFIX}n" # RESET style to default/normal
-TAB=$(echo -e '\t')
+T=$(echo -e '\t')
 # color keys
 cs=(black red green yellow blue magenta cyan white)
 declare -A colors # associative array (dict)
@@ -122,6 +138,8 @@ done
 
 while [[ ${input} != "0" ]]; do
 
+  equiv=$(curl -s -H "X-MBX-APIKEY: ${APIKEY}" "${URL}/${SPOT_API}/ticker/price?symbol=${CURRENCY}USDT" | jq '.price|tonumber')
+
   querystr="timestamp=$((($(date +%s) * 1000)))"
   sig=$(echo -n "$querystr" | openssl dgst -sha256 -hmac "${APISECRET}" | cut -c 10-)
   sig="signature=$sig"
@@ -132,89 +150,118 @@ while [[ ${input} != "0" ]]; do
   mapfile -t coins <<<"$(jq -r '. | keys_unsorted | .[]' <<<"${accountq}")"
 
   declare -A assets # associative array (dict)
+  qty_max=0
   # add key-value pairs of coins-amounts
   for i in "${!coins[@]}"; do
     coin=${coins[$i]}
     amount=${amounts[$i]}
-    assets[$coin]=$amount
+    # transform to 8 decimal (also to avoid exponentials)
+    amount=$(printf "%.8f" "${amount}")
+    max=${#amount}
+    if ((max > qty_max)); then
+      qty_max=${max}
+    fi
+    assets[$coin]=${amount}
   done
 
   declare -A prices
   declare -A changes
+  declare -A changecs
+  price_max=0
+  change_max=0
   for coin in "${!assets[@]}"; do
     symbol="${coin}USDT"
     mapfile -t pricper <<<"$(curl -s -H "X-MBX-APIKEY: ${APIKEY}" "${URL}/${SPOT_API}/ticker/24hr?symbol=${symbol}" | jq '. | [(.lastPrice|tonumber), (.priceChangePercent|tonumber)] | .[]')"
-    prices[${coin}]=${pricper[0]}
-    changes[${coin}]=${pricper[1]}
+    price=$(echo "${pricper[0]} / ${equiv}" | bc -l)
+    price=$(printf "%.8f" "${price}")
+    change=$(printf "%.2f" "${pricper[1]}")
+    if (($(echo "${change} < 0" | bc -l))); then
+      color=${colors[red]}
+      sign=""
+    else
+      color=${colors[green]}
+      sign="+"
+    fi
+    change="${sign}${change}"
+    max=${#price}
+    if ((max > price_max)); then
+      price_max=${max}
+    fi
+    max=${#change}
+    if ((max > change_max)); then
+      change_max=${max}
+    fi
+    prices[${coin}]=${price}
+    changecs[${coin}]=${color}
+    changes[${coin}]=${change}
   done
-
-  equiv=$(curl -s -H "X-MBX-APIKEY: ${APIKEY}" "${URL}/${SPOT_API}/ticker/price?symbol=EURUSDT" | jq '.price|tonumber')
 
   total=0
-  to_print="${REVERSE}${BOLD}COIN${TAB}QTY${TAB}${TAB}PRICE${TAB}${TAB} %24H↑↓${TAB}%ALLOC${TAB}VALUE${TAB}    ${UREVERSE}\n"
-  line=""
-  for i in {0..65}; do
-    line="${line}-"
-  done
-  to_print="${to_print}${UBOLD}"
   # calculate total
   for coin in "${coins[@]}"; do
     qty=${assets[$coin]}
-    value=$(echo "${qty} * ${prices[$coin]} / ${equiv}" | bc -l)
+    value=$(echo "${qty} * ${prices[$coin]}" | bc -l)
     total=$(echo "${total} + ${value}" | bc -l)
   done
+
+  total_l=$(printf "%.2f" "${total}")
+  total_l=${#total_l}
+
+  declare -A percs
+  declare -A values
+  # calculate allocation (%), format allocation and value
+  for coin in "${coins[@]}"; do
+    qty=${assets[$coin]}
+    value=$(echo "${qty} * ${prices[$coin]}" | bc -l)
+    perc=$(echo "${value} * 100.0 / ${total}" | bc -l)
+    values[${coin}]=$(printf "%.2f" "${value}")
+    percs[${coin}]=$(printf "%.2f" "${perc}")
+  done
+
+  COIN_LBL=$(printf "%-*s" "4" "COIN")
+  CHANGE_LBL=$(printf "%*s" "${change_max}" "%24H↑↓")
+  PRICE_LBL=$(printf "%*s" "${price_max}" "${CURRENCY}")
+  QTY_LBL=$(printf "%*s" "${qty_max}" "QTY")
+  TOTAL_LBL=$(printf "%*s" "${total_l}" "TOTAL")
+  header="${REVERSE}${BOLD}${COIN_LBL}${T}${CHANGE_LBL}${T}${PRICE_LBL}${T}${QTY_LBL}${T}%ALLOC${T}${TOTAL_LBL}${UREVERSE}${UBOLD}\n\n"
+  to_print="${header}"
+
+  width=0
   # prepare values to display
   for coin in "${coins[@]}"; do
+    # get and add right-justify values
     asset="${BOLD}$(printf "%s" "${coin}")${UBOLD}"
-    qty=${assets[${coin}]}
-    change=${changes[${coin}]}
-    if (($(echo "${change} < 0" | bc -l))); then
-      changec=red
-      sign=""
-    else
-      changec=green
-      sign="+"
+    changec=${changecs[${coin}]}
+    change=$(printf "%*s" "${change_max}" "${changes[${coin}]}")
+    price=$(printf "%*s" "${price_max}" "${prices[$coin]}")
+    qty=$(printf "%*s" "${qty_max}" "${assets[${coin}]}")
+    perc=$(printf "%*s" "6" "${percs[${coin}]}")
+    value=$(printf "%*s" "${total_l}" "${values[${coin}]}")
+    # fill out row with values
+    row="${asset}${T}${changec}${change}${RESET}${T}${price}${T}${qty}${T}${perc}${T}${value}"
+    # get row length, store the longest number of characters
+    charnum=${#row}
+    if ((charnum > width)); then
+      width=${charnum}
     fi
-    change="${sign}$(printf "%.2f" "${change}")"
-    # fix change padding with spaces
-    integers=$(echo "${change}" | cut -d'.' -f1)
-    integers=${#integers}
-    lack=$((3 - integers))
-    for i in $(eval "echo {0..${lack}}"); do
-      change=" ${change}"
-    done
-    price=${prices[$coin]}
-    value=$(echo "${qty} * ${price} / ${equiv}" | bc -l)
-    perc=$(echo "${value} * 100.0 / ${total}" | bc -l)
-    perc=$(printf "%.2f" "${perc}")
-    # fix perc padding with spaces
-    integers=$(echo "${perc}" | cut -d'.' -f1)
-    integers=${#integers}
-    lack=$((2 - integers))
-    for i in $(eval "echo {0..${lack}}"); do
-      perc=" ${perc}"
-    done
-    # fix qty decimals with spaces
-    decimals=$(echo "${qty}" | cut -d'.' -f2)
-    decimals=${#decimals}
-    lack=$((8 - decimals))
-    for i in $(eval "echo {0..${lack}}"); do
-      qty="${qty} "
-    done
-    # fix qty decimals with spaces
-    decimals=$(echo "${price}" | cut -d'.' -f2)
-    decimals=${#decimals}
-    lack=$((8 - decimals))
-    for i in $(eval "echo {0..${lack}}"); do
-      price="${price} "
-    done
-    to_print="${to_print}${asset}${TAB}${qty}${TAB}€${price}${TAB}${colors[${changec}]}${change}${colors[black]}${TAB}${perc}${TAB}€$(printf "%.2f" "${value}")\n\n"
+    to_print="${to_print}${row}\n\n"
   done
+  # build line
+  line_l=$((width - 5))
+  line=""
+  for i in $(eval "echo {0..${line_l}}"); do
+    line="${line}-"
+  done
+  to_print="${to_print}${BOLD}${line}\n"
   # ternary operator: green if bigger than investment, red otherwise
   (($(echo "${total} > ${INVESTMENT}" | bc -l))) && totalc=green || totalc=red
-  to_print="${to_print}${BOLD}${line}\n"
-  to_print="${to_print}TOTAL${TAB}${TAB}${TAB}${TAB}${TAB}${TAB}${TAB}${UBOLD}${colors[${totalc}]}€$(printf "%.2f" "${total}")${RESET}"
-  height=$((($(awk -F"n" '{print NF-1}' <<<"${to_print}") + 3)))
+  total=$(printf "%.2f" "${total}")
+  last_row="${T}${UBOLD}${colors[${totalc}]}${total}${RESET}"
+  last_row=$(printf "%*s" "${width}" "${last_row}")
+  to_print="${to_print}${last_row}"
+
+  height=$(($(awk -F"n" '{print NF-1}' <<<"${to_print}") - 6))
+  width=$((width + 6))
   clear
   dialog \
     --colors \
@@ -222,7 +269,7 @@ while [[ ${input} != "0" ]]; do
     --no-mouse \
     --backtitle "Binance Spot Assets Balance" \
     --title "Total Balance" \
-    --infobox "${to_print}" ${height} 0
+    --infobox "${to_print}" "${height}" "${width}"
 done
 
 echo # to get a newline after quitting
